b2a96fd5d9c06d77297eb52b892a1c3d4f54f94f
meekis <ivyvivi@hotmail.no>
2025-04-11 21:08:30 +0200

    I dunno

--- server/server.ts
+++ server/server.ts
@@ -10,15 +10,18 @@ app.get("/api/hello", async (c) => {
 });
 app.get("/01_heroku/api/skoler", async (c) => {
   const result = await postgresql.query(
-    "select skolenavn, besoksadresse_besoksadresse_postnummer, from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole",
+    `select
+         skolenavn, eierforhold, besoksadresse_besoksadresse_adressenavn, besoksadresse_besoksadresse_postnummer, besoksadresse_besoksadresse_poststed
+          skolenavn, eierforhold, besoksadresse_besoksadresse_adressenavn, besoksadresse_besoksadresse_postnummer, besoksadresse_besoksadresse_poststed,
+         st_transform(posisjon, 4326)::json as posisjon
+       from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole
+      `,
   );
   return c.json({
     type: "FeatureCollection",
     crs: {
       type: "name",
-      properties: {
-        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
-      },
+      properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
     },
     features: result.rows.map(
       ({ geometry: { coordinates }, ...properties }) => ({
